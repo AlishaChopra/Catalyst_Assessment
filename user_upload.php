0cd3748894038ae6f268067466d3374af4b7baf2
Alisha Chopra <alisha.s.chopra@gmail.com>
2021-01-12 01:01:45 -0500

    Validations and code changes

--- user_upload.php
+++ user_upload.php
@@ -4,55 +4,58 @@ class CreateAndStoreUserDetail {
     public $data;
     public $count;
     public $errorLog;
-    //public $invalidEmailCount;
+    public $options;
     public function getOption()
     {
+        $this->count = 0;
+        $this->errorLog = " ";
         $shortopts  = "";
         $shortopts .= "u:";  
         $shortopts .= "p:"; 
-        $shortopts .= "h:";
-        $this->count = 0;
-        $this->errorLog = " ";
+        $shortopts .= "h:"; 
         $longopts  = array(
             "file:",    
             "create_table::",   
             "dry_run::",
             "help::"
         );
-        $options = getopt($shortopts, $longopts);
-        var_dump($options);
-        if(array_key_exists("file",$options) && array_key_exists("dry_run",$options) )
+        $this->options = getopt($shortopts, $longopts);
+        if(array_key_exists("file",$this->options) && array_key_exists("dry_run",$this->options))
         {
-            if(isset($options["file"]))
+            if($this->validate("file"))
             {
-                $csvData=$this->readCSV($options["file"]);
-                echo "dry run completed";
+                if(!$this->readCSV($this->options["file"]))
+                    echo "CSV file does not exists";
+                else{
+                    for($c=0; $c<count($this->csvData);$c++)
+                    {
+                        if($this->validateEmail($this->csvData[$c]))
+                        {
+                            $this->count = $this->count + 1;
+                        }
+                    }
+                    echo $this->count. " Out of " . count($this->csvData) . " Users have valid email\n";
+                    echo count($this->csvData)-$this->count . " Users does not have  valid email\n";
+                }
             }
             else
             {
                 echo "File Name is not provided";
             }
         }
-        elseif(array_key_exists("create_table",$options))
+        elseif(array_key_exists("create_table",$this->options))
         {
-            if(isset($options["u"]) && isset($options["p"]) && isset($options["h"]))
+            if($this->validate("u") && $this->validate("p") && $this->validate("h"))
             {
-                $db = $this->databaseConnectivity($options["u"],$options["p"], $options["h"]);
+                $db = $this->databaseConnectivity($this->options["u"],$this->options["p"], $this->options["h"]);
                 if(!$db) 
-                {
                     echo "Error : Unable to open database\n";
-                } 
                 else 
                 {
-                    $result=$this->createTable($db);
-                    if($result)
-                    {
+                    if($this->createTable($db))
                         echo "Table created successfully";
-                    }
                     else
-                    {
                         echo pg_last_error($db);
-                    }
                     pg_close($db);
                 }
             }
@@ -60,55 +63,60 @@ class CreateAndStoreUserDetail {
                 echo "Hostname, username, and password is not provided";
             }   
         } 
-        elseif(array_key_exists("file",$options))
+        elseif(array_key_exists("file",$this->options))
         {
-            if(isset($options["file"]))
+            if($this->validate("file"))
             {
-                $csvData=$this->readCSV($options["file"]);
+                if($this->readCSV($this->options["file"]))
+                {
+                    if($this->validate("u") && $this->validate("p") && $this->validate("h"))
+                    {
+                        $db = $this->databaseConnectivity($this->options["u"],$this->options["p"], $this->options["h"]);
+                        if($db) 
+                        {
+                            if($this->createTable($db))
+                            {
+                                for($c=0; $c<count($this->csvData);$c++)
+                                {
+                                    if($this->validateEmail($this->csvData[$c]))
+                                    {
+                                        $this->insertData($db,$this->csvData[$c]);
+                                    }
+                                    else
+                                    {
+                                        $this->errorLog = $this->errorLog. $this->csvData[$c]["name"] . " " . $this->csvData[$c]["surname"]. " ". $this->csvData[$c]["email"] . " invalid email\n" ;
+                                    }
+                                }
+                                echo $this->count. " Out of " . count($this->csvData) . " Processed Successfully\n";
+                                echo "Error log: \n" .   $this->errorLog;
+                                pg_close($db);           
+                            }   
+                        }
+                    } 
+                    else
+                    {
+                        echo "Hostname, username, and password is not provided";
+                    }       
+                }
             }
             else
             {
                 echo "File name is not provided";
             }
-            
-            if(isset($options["u"]) && isset($options["p"]) && isset($options["h"]))
-            {
-                $db = $this->databaseConnectivity($options["u"],$options["p"], $options["h"]);
-                if($db) 
-                {
-                    $result = $this->createTable($db);
-                    if($result)
-                    {
-                        for($c=0; $c<count($csvData);$c++)
-                        {
-                            $data = $csvData[$c];
-                            if(filter_var($data["email"], FILTER_VALIDATE_EMAIL))
-                            {
-                                $this->insertData($db,$data,count($csvData));
-                            }
-                            else
-                            {
-                                $this->errorLog = $this->errorLog. $data["name"] . " " . $data["surname"]. " ". $data["email"] . " invalid email\n" ;
-                            }
-                        }
-                        echo $this->count. " Out of " . count($csvData) . " Processed Successfully\n";
-                        echo "Error log: \n" .   $this->errorLog;
-                    pg_close($db);           
-                    }   
-                }
-            } 
-            else
-            {
-                echo "Hostname, username, and password is not provided";
-            }
-               
         }   
-        /*elseif(array_key_exists("help",$options))
+        elseif(array_key_exists("help",$this->options))
         {
-
-        }*/   
+            echo "
+            --file [csv file name] – this is the name of the CSV to be parsed\n
+            --create_table – this will cause the PostgreSQL users table to be built (and no further action
+            will be taken)\n
+            --dry_run – this will be used with the --file directive in case we want to run the script but not
+            insert into the DB. All other functions will be executed, but the database won't be altered\n
+            -u – PostgreSQL username\n
+            -p – PostgreSQL password\n
+            -h – PostgreSQL host";
+        }   
     }
-
     public function readCSV($filename)
     {
         if(!file_exists($filename) || !is_readable($filename))
@@ -117,27 +125,25 @@ class CreateAndStoreUserDetail {
         if (($handle = fopen($filename, 'r')) !== FALSE)
         {
             while (($row = fgetcsv($handle)) !== FALSE)
-                {
-                    if(!$header)
-                        $header = array_map('trim', $row);
-                    else
-                        $csvData[] = array_combine($header, $row);
-                }
-                fclose($handle);
-                return TRUE;     
+            {
+                if(!$header)
+                    $header = array_map('trim', $row);
+                else
+                    $this->csvData[] = array_combine($header, $row);
+            }
+            fclose($handle);
+            return TRUE;     
         }
     }
-
     public function databaseConnectivity($username,$password,$hostvalue)
     {
         $host        = "host = ".$hostvalue."";
         $port        = "port = 5432";
-        $dbname      = "dbname = userDb";
+        $dbname      = "dbname = postgres";
         $credentials = "user = ".$username." password=".$password."";
         $db = pg_connect( "$host $port $dbname $credentials");
         return $db;
     }
-
     public function createTable($db) 
     {    
         $sql ="CREATE TABLE IF NOT EXISTS users
@@ -145,15 +151,14 @@ class CreateAndStoreUserDetail {
         surname character varying(255),
         email character varying(255) UNIQUE);";
         $ret = pg_query($db, $sql);
-        //pg_close($db);
         return $ret;    
     }
-    public function insertData($db, $data, $totalRecords)
+    public function insertData($db, $data)
     {
         $name = ucwords(strtolower(pg_escape_string($data["name"])));
         $surname = ucwords(strtolower(pg_escape_string($data["surname"])));
         $email = strtolower((pg_escape_string($data["email"])));
-        $sql ="SELECT * from USERS WHERE email='$email'";
+        $sql ="SELECT * from USERS WHERE email='$email'"; // check whether an email exists before inserting
         $result = pg_query($db, $sql);
         $rows = pg_num_rows($result);
         if($rows==0) 
@@ -175,7 +180,21 @@ class CreateAndStoreUserDetail {
             $this->errorLog = $this->errorLog. $name . " " . $surname . " " . $email . " User exists" .  "\n";
         }
     }
+    public function validate($cmdOption)
+    {
+        if(isset($this->options[$cmdOption]))
+            return TRUE;
+        else
+            return FALSE;    
+    }
+    public function validateEmail($data)
+    {
+        if(filter_var($data["email"], FILTER_VALIDATE_EMAIL))
+            return TRUE;
+        else
+            return FALSE;
+    }
 }
-$apple = new CreateAndStoreUserDetail();
-$apple->getOption();
+$userDetail = new CreateAndStoreUserDetail();
+$userDetail->getOption();
 ?>
